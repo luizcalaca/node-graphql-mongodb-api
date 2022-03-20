9ce8bf4b0f1f5c2f10fc41a7d97b40f44d06d4bc
Luiz Calaça <luizcalaca@Luizs-MacBook-Air.local>
2022-03-20 10:49:02 -0300

    refactor: delete database const with environment variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,11 @@
 require("dotenv").config();
-
 const mongoose = require("mongoose");
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-// Database
-const db = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
-  name: process.env.DB_NAME,
-};
-
-const dbUri = `mongodb://127.0.0.1:27017/blog`;
+const dbUri = process.env.DB_HOST
 const dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
